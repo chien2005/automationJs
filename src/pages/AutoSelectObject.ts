f9fea4cfd71a63287f45d8d6f865ce2750a1ca9d
chien2005 <email-dang-dung-tren-github>
2025-10-21 20:00:46 +0700

    feat: add AutoSelectObject page and update imports in OrderPage; clean up Bai2 test

--- src/pages/AutoSelectObject.ts
+++ src/pages/AutoSelectObject.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+    // 1) Login
+    const login = new LoginPage(page);
+    await login.goto(cfg.baseUrl);
+    await login.loginNpp(cfg.username, cfg.password);
 
 });
